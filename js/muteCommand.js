2e9b812e368964a326e0e73a2ebd2c82b793a647
Philipp Westermann <vanemiera@gmail.com>
2018-10-19 22:48:55 +0200

    Fix mute command

--- js/muteCommand.js
+++ js/muteCommand.js
@@ -18,15 +18,27 @@ var MuteCommand = function (parent) {
         try {
           var guild = self.parent.bot.guilds.get(mutee.guildID);
           var channel = guild.channels.get(mutee.channelID);
-          var member = guild.members.get(mutee.memberID);
-          var role = guild.roles.filter(r => r.name == MUTE_ROLE).first();
-          if (member.roles.has(role.id)) {
-            member.removeRole(role);
-            channel.send('Member <@' + mutee.memberID + '> muted by <@' + mutee.modID + '> can now talk again.');
-          }
-          self.mutes.splice(i, 1);
-          self.save();
-          console.log('Removed ' + mutee.memberName + ' from the list');
+          guild.fetchMember(mutee.memberID)
+            .then(function (member) {
+              var role = guild.roles.filter(r => r.name === MUTE_ROLE).first();
+
+              member.removeRole(role)
+                .then(function () {
+                  member.addRoles(mutee.ownedRoles.map(r => r.id))
+                })
+                .then(function () {
+                  channel.send('Member <@' + mutee.memberID + '> muted by <@' + mutee.modID + '> can now talk again.');
+                })
+                .catch(function (e) {
+                  console.log(e);
+                });
+              self.mutes.splice(i, 1);
+              self.save();
+              console.log('Removed ' + mutee.memberName + ' from the list');
+            })
+            .catch(function (e) {
+              console.log(e);
+            });
         } catch (error) {
           console.log(error)
         }
@@ -46,7 +58,7 @@ MuteCommand.prototype.handleCommand = function (message) {
   }
 
   var roleID = false;
-  var mutee = {memberID: 0, timeout: 0, memberName: '', guildID: 0, guildName: '', modID: 0, modName: '', channelID: 0, channelName: '', ownedRoles: []};
+  var mutee = { memberID: 0, timeout: 0, memberName: '', guildID: 0, guildName: '', modID: 0, modName: '', channelID: 0, channelName: '', ownedRoles: [] };
   var now = new Date();
   var memberID = '';
   var hours = 0;
@@ -59,48 +71,60 @@ MuteCommand.prototype.handleCommand = function (message) {
     return;
   }
   try {
-    memberID = words[1].substr(2,18);
+    memberID = words[1].substr(2, 18);
     hours = parseFloat(words[2])
-  }catch (e){
+  } catch (e) {
     message.channel.send(USAGE);
     return
   }
 
-  var guildMember = message.guild.member(memberID);
-
-  mutee.memberID = memberID;
-  mutee.memberName = guildMember.user.username;
-  mutee.modID = message.author.id;
-  mutee.modName = message.author.username;
-  mutee.timeout = now.valueOf() + hours * MILLISECONDS_PER_HOUR;
-  mutee.channelID = message.channel.id;
-  mutee.channelName = message.channel.name;
-  mutee.guildID = message.channel.guild.id;
-  mutee.guildName = message.channel.guild.name;
-  mutee.ownedRoles = guildMember.roles
-    .map(x => ({id: x.id, name: x.name}))
-    .filter(r => !["@everyone", "Muted"].includes(r.name));
-  this.mutes.push(mutee);
-
-  this.save();
-
-  for (var [key, value] of message.guild.roles) {
-    if (value.name == MUTE_ROLE) {
-      roleID = key;
-      break;
-    }
-  }
-
-  if (!roleID) return;
-
   message.guild.fetchMember(memberID)
     .then(function (member) {
-      member.addRole(roleID);
-      modChannel.send('Member <@' + memberID + '> muted by ' + message.author + ' for ' + hours + ' hour(s).');
-    })
+
+      mutee.memberID = memberID;
+      mutee.memberName = member.user.username;
+      mutee.modID = message.author.id;
+      mutee.modName = message.author.username;
+      mutee.timeout = now.valueOf() + hours * MILLISECONDS_PER_HOUR;
+      mutee.channelID = message.channel.id;
+      mutee.channelName = message.channel.name;
+      mutee.guildID = message.channel.guild.id;
+      mutee.guildName = message.channel.guild.name;
+      mutee.ownedRoles = member.roles
+        .map(x => ({ id: x.id, name: x.name }))
+        .filter(r => !["@everyone", MUTE_ROLE].includes(r.name));
+      this.mutes.push(mutee);
+
+      this.save();
+
+      for (var [key, value] of message.guild.roles) {
+        if (value.name === MUTE_ROLE) {
+          roleID = key;
+          break;
+        }
+      }
+
+      if (!roleID) return;
+
+      member.addRole(roleID)
+        .then(function () {
+          member.removeRoles(mutee.ownedRoles.map(r => r.id));
+        })
+        .then(function () {
+          modChannel.send('Member <@' + memberID + '> muted by ' + message.author + ' for ' + hours + ' hour(s).');
+        })
+        .catch(function (e) {
+          console.log(e);
+        });
+
+    }.bind(this))
     .catch(function (e) {
       console.log(e);
     });
+};
+
+MuteCommand.prototype.muteMember = function (member) {
+
 };
 
 MuteCommand.prototype.save = function () {
